62f4205e4c306a5064484faa4ab67bb7e59c4cb9
MoovendhanR <moovendhanr1011@gmail.com>
2022-05-02 17:08:19 +0530

    first commit

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {BrowserRouter} from "react-router-dom";
+import {Provider as ReduxProvider} from 'react-router-dom';
+import {store} from './Redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ReduxProvider store={store} >
+   <BrowserRouter>
+   <App />
+   </BrowserRouter>
+   </ReduxProvider>
   </React.StrictMode>
 )
